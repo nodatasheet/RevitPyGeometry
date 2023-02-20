b5559e1a2c956c5468d66265f6815318f7e3206e
Konstantin <92411255+nodatasheet@users.noreply.github.com>
2023-02-20 11:18:26 +0800

    bkp

---
+++ src/geometry/__init__.py
@@ -0,0 +1,8 @@
+
+class GeometryEntity(Basic, EvalfMixin):
+    """The base class for all geometrical entities.
+
+    This class does not represent any particular geometric entity, it only
+    provides the implementation of some methods common to all subclasses.
+
+    """
---
+++ sympy/README.md
@@ -0,0 +1,1 @@
+Modules from [SymPy](https://github.com/sympy/sympy/), for the reference of base structure.
