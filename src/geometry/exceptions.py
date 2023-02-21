51f366460bb701a031ca92fe434c06598faf5c3c
Konstantin <92411255+nodatasheet@users.noreply.github.com>
2023-02-21 15:40:09 +0800

    add exceptions

--- src/geometry/exceptions.py
+++ src/geometry/exceptions.py
@@ -11,5 +11,24 @@ class ValidationException(GeometryException):
     pass
 
 
+class TypeError(GeometryException):
+    pass
+
+
 class TypeValidationException(GeometryException):
     pass
+
+
+def raise_wrong_type(obj, expected_type):
+    # type: (object, type) -> None
+    raise TypeValidationException(
+        'Expected <{}>, got <{}>'.format(expected_type.__name__,
+                                         type(obj).__name__)
+    )
+
+
+def raise_wrong_attr_qty(qty_expected, qty_got):
+    # type: (int, int) -> None
+    raise TypeError(
+        'expected {} arguments, got {}'.format(qty_expected, qty_got)
+    )
